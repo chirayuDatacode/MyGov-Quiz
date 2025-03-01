f4019f3abe2c70da0e1ed036afd524d6143db492
chirayuDatacode <chirayu.datacode@gmail.com>
2025-03-01 12:06:23 +0530

    initial commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -14,15 +14,15 @@ export default function Footer(){
                 <div className="server-info-right"><span><strong>Last Updated: Feb, 2025 -aexa-quizweb01</strong></span></div>
             </div>
             <div className="hor_ruler col-lg-10 pt-0"><hr /></div>
-            <div className="col-lg-10 d-flex justify-content-between align-items-center mb-2 bg-primary">
-                <div className="row  bg-danger w-100 d-flex justify-content-between">
-                  <div className="col-6 bg-success">
+            <div className="col-lg-10 d-flex justify-content-between align-items-center mb-2 ">
+                <div className="row  w-100 d-flex justify-content-between">
+                  <div className="col-6 ">
                     <div className="d-flex  ">
                     <img height="50px" src="https://quiz.mygov.in/wp-content/themes/quizz/assets/images/logo-nic.png" />
                     <p className="my-1 mx-1 text-white">© Content owned, updated and maintained by the <a className="text-white" href="https://www.mygov.in/" title="MyGov (External link will open in new window)" target="_blank">MyGov</a> Cell. This website belongs to <a href="https://www.mygov.in/" title="MyGov (External link will open in new window)" target="_blank">MyGov</a>, <a href="https://www.meity.gov.in/" title="Ministry of Electronics &amp; Information Technology (External link will open in new window)" target="_blank">Ministry of Electronics &amp; Information Technology</a>, <a href="https://www.india.gov.in/" title="Government of India. (External link will open in new window)" target="_blank">Government of India</a>. <br/>Platform is designed, developed and hosted by <a href="https://www.nic.in" title="NIC Logo (External link will open in new window)" target="_blank">National Informatics Centre.</a></p>
                     </div>
                   </div>
-                  <div className="col-6 bg-success">
+                  <div className="col-6 ">
                   <div class="social-content">
                     <p>Follow us</p>
                     <a href="https://twitter.com/mygovindia" target="_blank" title="MyGov-Twitter" class="jquery-once-4-processed"><i class="twitter-icon">MyGov-Twitter</i></a>
